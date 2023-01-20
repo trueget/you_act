4be95f2e654e24f8ecd2f598ee6ffb37bc6c18b5
roma <trueget33@gmail.com>
2023-01-20 15:56:31 +0300

    add board form

--- you_act/tasks/serializers.py
+++ you_act/tasks/serializers.py
@@ -3,7 +3,9 @@ from tasks.models import Board
 
 
 class BoardSerializer(serializers.ModelSerializer):
+    name_board = serializers.CharField(max_length=100, default='My project')
 
     class Meta:
         model = Board
         fields = '__all__'
+        # fields = ('name_board', 'owner')
--- you_act/tasks/views.py
+++ you_act/tasks/views.py
@@ -19,19 +19,28 @@ class BoardDetailView(APIView):
     template_name = 'my_workspace.html'
 
     def get(self, request):
-        board = get_object_or_404(Board, owner=request.user)
-        # board = Board.objects.get(owner=request.user)
+        # board = get_object_or_404(Board, owner=request.user)
+        board = Board.objects.filter(owner=request.user)
         serializer = BoardSerializer(board)
-        return Response({'serializer': serializer, 'board': board})
+        return Response({'serializer': serializer, 'all_boards': board})
 
     def post(self, request):
-        board = get_object_or_404(Board, owner=request.user)
-        serializer = BoardSerializer(board)
-        print('-'*50)
-        print(serializer)
+        print('POST запустился---------------')
+        # board = get_object_or_404(Board, owner=request.user)
+        board = Board.objects.filter(owner=self.request.user)
+        # board = Board.objects.get(owner=self.request.user)
+        # serializer = BoardSerializer(board, data=request.data)
+        serializer = BoardSerializer(data=request.data)
+
         if not serializer.is_valid():
-            return Response({'serializer': serializer, 'board': board})
+            print('не валидна ------------------')
+            # return Response({'serializer': serializer, 'boards': board})
+            return Response(serializer)
+        print('пытаемся сохранить ------------------')
+        # serializer.owner
         serializer.save
+        print('сохранили ------------------')
+        print(serializer)
         return redirect('/')
 
 
