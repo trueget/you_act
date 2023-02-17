f7c60260fb29f9ffb74d8c05f96e76db089ee020
roma <trueget33@gmail.com>
2023-02-17 10:07:52 +0300

    add style on modal

--- you_act/tasks/forms.py
+++ you_act/tasks/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput
 from tasks.models import Tasks, Column
 
 
@@ -8,9 +8,25 @@ class ColumnForm(ModelForm):
         model = Column
         fields = ['name_column']
 
+        widgets = {
+            'name_column': TextInput(attrs={
+                'placeholder': 'Enter column name...',
+            })
+        }
+
 
 class TasksForm(ModelForm):
 
     class Meta:
         model = Tasks
         fields = ['tittle', 'description']
+
+        widgets = {
+            'tittle': TextInput(attrs={
+                'placeholder': 'Enter task name...',
+            }),
+            'description': Textarea(attrs={
+                'placeholder': 'Describe the task...',
+                'rows': 4
+            })
+        }
